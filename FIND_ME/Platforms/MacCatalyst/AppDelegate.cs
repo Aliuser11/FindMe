0fc6af2e6f998aef91e2579cd208d91b626477e3
Aliuser11 <dajmispokojAZ@gmail.com>
2024-04-17 13:13:54 +0200

    Add project files.

---
+++ FIND_ME/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FIND_ME
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
