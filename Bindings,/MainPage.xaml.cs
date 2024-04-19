50c366bc4d73fa863ba994672c2084e98e30562b
Aliuser11 <dajmispokojAZ@gmail.com>
2024-04-19 13:20:34 +0200

    project added;

---
+++ Bindings,/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Bindings
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
