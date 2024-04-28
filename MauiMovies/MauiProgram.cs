0c11dca58f261ccd583b17f4e05940aea5be7440
Aliuser11 <dajmispokojAZ@gmail.com>
2024-04-28 19:14:53 +0200

    MauiMovies  => continuation;

--- MauiMovies/MauiProgram.cs
+++ MauiMovies/MauiProgram.cs
@@ -1,22 +1,21 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
 
-namespace MauiMovies
+namespace MauiMovies;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .UseMauiCommunityToolkit();
 
-
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
