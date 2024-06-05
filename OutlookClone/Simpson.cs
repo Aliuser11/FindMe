1ba82e9894a9c0af4bb5dff12b5e950bb85a1233
Aliuser11 <dajmispokojAZ@gmail.com>
2024-06-05 16:42:00 +0200

    OutlookClone;

---
+++ OutlookClone/Simpson.cs
@@ -0,0 +1,10 @@
+﻿namespace OutlookClone
+{
+    class Simpson
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+        public string image { get; set; }
+        public string characterDirection { get; set; }
+    }
+}
